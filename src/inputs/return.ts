7d9d8d0d924f5722032447b38ad3816be0d8bad2
ftmhabt <ftmh.a78@gmail.com>
2025-04-24 11:21:45 +0330

    Change return command

--- src/inputs/return.ts
+++ src/inputs/return.ts
@@ -1,6 +1,6 @@
 import { QuestionAsync } from ".";
 
 export default async function AskReturn(): Promise<boolean> {
-  const ret = await QuestionAsync("\nTo go back to menu press y: ");
-  return ret.trim().toLowerCase() === "y";
+  const ret = await QuestionAsync("\nTo go back to menu press 0: ");
+  return ret.trim() === "0";
 }
