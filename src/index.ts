794f44d57b69c1ffefb6296795b9f5cb4e2697f2
ftmhabt <ftmh.a78@gmail.com>
2025-05-29 12:47:32 +0330

    Handle error

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,12 @@
 import { App } from "./cli/app";
 import { initCategories } from "./services/categoryRepository";
 import { initContacts } from "./services/contactRepository";
+import { HandleError } from "./utils";
 
 try {
   initContacts();
   initCategories();
   App();
-} catch (err) {}
+} catch (err) {
+  HandleError(err);
+}
